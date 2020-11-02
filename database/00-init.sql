9437e1cd589d72194f00276d8da13a67d996ff89
plump_albert <plumpalbert@gmail.com>
2020-11-03 00:09:00 +0300

    Initial commit

---
+++ database/00-init.sql
@@ -0,0 +1,32 @@
+CREATE TABLE "USER" (
+  "user_id" SERIAL PRIMARY KEY NOT NULL,
+  "user_name" varchar NOT NULL,
+  "user_login" varchar NOT NULL,
+  "user_password" char[256] NOT NULL,
+  "user_type" smallint NOT NULL,
+  "group_id" int REFERENCES "GROUP"
+);
+
+CREATE TABLE "GROUP" (
+  "group_id" SERIAL PRIMARY KEY NOT NULL,
+  "group_faculty" varchar NOT NULL,
+  "group_specialty" varchar NOT NULL,
+  "group_year" int NOT NULL
+);
+
+CREATE TABLE "SUBJECT" (
+  "subject_id" SERIAL PRIMARY KEY NOT NULL,
+  "subject_audience" varchar NOT NULL,
+  "subject_type" smallint NOT NULL,
+  "subject_name" varchar NOT NULL,
+  "subject_time" time NOT NULL,
+  "subject_weekday" smallint NOT NULL,
+  "subject_weektype" smallint NOT NULL,
+  "subject_teacher" int REFERENCES "USER"
+);
+
+CREATE TABLE "VISITS" (
+  "group_id" int REFERENCES "GROUP" ON DELETE CASCADE ON UPDATE CASCADE,
+  "subject_id" int REFERENCES "SUBJECT" ON DELETE CASCADE ON UPDATE CASCADE,
+  PRIMARY KEY ("group_id", "subject_id")
+);
